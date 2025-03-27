063ff893fcc8bc40f5703abececaed6fb8ac2f7a
nadia <nadia.torres@tecsup.edu.pe>
2025-03-27 16:13:38 -0500

    Commit inicial del proyecto Laravel

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!-- filepath: resources/views/welcome.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Bienvenido a Laravel</h1>
+</body>
+</html>
